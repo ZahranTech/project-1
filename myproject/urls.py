94315cb536c44ac73f5ef341638982ff6643d1cf
ZahranTech <alkhaled123455@gmail.com>
2025-09-02 05:41:05 -0700

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path("", lambda request: HttpResponse("مرحباً بك في المشروع 🚀")),
     path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
